da984704faf2836dd7f8458eee36b93f59da2712
cmos3511 <gary3511@gmail.com>
2017-12-01 19:32:32 +0800

    added proj and block post feature; updated the compat feature with firefox browser

--- python/op_site/be_rpt/urls.py
+++ python/op_site/be_rpt/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.ProjList.as_view(), name="proj_list"),
-    url(r"^(?P<proj_name>\w+)/$", views.BlockList.as_view(), name="block_list"),
-    url(r"^(?P<proj_name>\w+)/(?P<block_name>\w+)/$", views.VersionList.as_view(), name="version_list"),
-    url(r"^(?P<proj_name>\w+)/(?P<block_name>\w+)/(?P<version_name>\w+)/$", views.VersionDetail.as_view(), name="version_detail"),
+    url(r"^show/$", views.ProjList.as_view(), name="proj_list"),
+    url(r"^show/(?P<proj_name>\w+)/$", views.BlockList.as_view(), name="block_list"),
+    url(r"^show/(?P<proj_name>\w+)/(?P<block_name>\w+)/$", views.VersionList.as_view(), name="version_list"),
+    url(r"^show/(?P<proj_name>\w+)/(?P<block_name>\w+)/(?P<version_name>\w+)/$", views.VersionDetail.as_view(), name="version_detail"),
+    url(r"^post/project/$", views.ProjPost.as_view(), name="post_project"),
+    url(r"^post/block/$", views.BlockPost.as_view(), name="post_block"),
+    # url(r"^post/version/$", views.VersionPost.as_view(), name="post_version"),
 ]
--- python/op_site/be_rpt/views.py
+++ python/op_site/be_rpt/views.py
@@ -1,7 +1,13 @@
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.utils.decorators import method_decorator
 from .models import User, Title, Proj, Block, Version
 from django.utils import timezone as tz
+import json
 
 # Create your views here.
 class ProjList(ListView):
@@ -55,3 +61,35 @@ class VersionDetail(DetailView):
         context["version_name"] = self.version_name
         context['now'] = tz.now()
         return context
+
+@method_decorator(csrf_exempt, name="dispatch")
+class ProjPost(View):
+    """be report post project data"""
+    def post(self, request, *args, **kwargs):
+        proj_dic = json.loads(request.body.decode()).get("proj_dic", {})
+        if not proj_dic:
+            return HttpResponseBadRequest("input proj_dic is NA")
+        proj_obj, create_flg = Proj.objects.update_or_create(
+            {"name": proj_dic.get("name"), "data": proj_dic.get("data")},
+            name=proj_dic.get("name"))
+        return HttpResponse(
+            json.dumps({"proj_name": proj_obj.name, "create_flg": create_flg}),
+            content_type="application/json")
+
+@method_decorator(csrf_exempt, name="dispatch")
+class BlockPost(View):
+    """be report post block data"""
+    def post(self, request, *args, **kwargs):
+        block_dic = json.loads(request.body.decode()).get("block_dic")
+        if not block_dic:
+            return HttpResponseBadRequest("input block_dic is NA")
+        proj_name = block_dic.get("proj")
+        proj_obj = Proj.objects.get(name=proj_name)
+        if not proj_obj:
+            return HttpResponseBadRequest("project object is NA")
+        block_obj, create_flg = Block.objects.update_or_create(
+            {"name": block_dic.get("name"), "proj": proj_obj, "data": block_dic.get("data")},
+            name=block_dic.get("name"), proj=proj_obj)
+        return HttpResponse(
+            json.dumps({"block_name": block_obj.name, "create_flg": create_flg}),
+            content_type="application/json")

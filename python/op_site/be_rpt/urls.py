4e8247767df56874668ae327bce09db347166e0b
cmos3511 <gary3511@gmail.com>
2017-12-01 12:07:04 +0800

    completed op_site basic page framework

--- python/op_site/be_rpt/urls.py
+++ python/op_site/be_rpt/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r"^$", views.ProjList.as_view(), name="proj_list"),
     url(r"^(?P<proj_name>\w+)/$", views.BlockList.as_view(), name="block_list"),
+    url(r"^(?P<proj_name>\w+)/(?P<block_name>\w+)/$", views.VersionList.as_view(), name="version_list"),
+    url(r"^(?P<proj_name>\w+)/(?P<block_name>\w+)/(?P<version_name>\w+)/$", views.VersionDetail.as_view(), name="version_detail"),
 ]
--- python/op_site/be_rpt/views.py
+++ python/op_site/be_rpt/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import User, Title, Proj, Block, Version
+from django.utils import timezone as tz
 
 # Create your views here.
 class ProjList(ListView):
@@ -14,7 +15,43 @@ class ProjList(ListView):
 class BlockList(ListView):
     """be report block list"""
     model = Block
+    def get_queryset(self):
+        self.proj_name = self.kwargs.get("proj_name")
+        return Block.objects.filter(proj__name=self.proj_name)
     def get_context_data(self, **kwargs):
-        context = super(ProjList, self).get_context_data(**kwargs)
-        a = self.kwargs['proj_name']
+        context = super(BlockList, self).get_context_data(**kwargs)
+        context["block_head_lst"] = Title.objects.first().block
+        context["proj_name"] = self.proj_name
+        return context
+
+class VersionList(ListView):
+    """be report version list"""
+    model = Version
+    def get_queryset(self):
+        self.proj_name = self.kwargs.get("proj_name")
+        self.block_name = self.kwargs.get("block_name")
+        return Version.objects.filter(
+            block__name=self.block_name, block__proj__name=self.proj_name)
+    def get_context_data(self, **kwargs):
+        context = super(VersionList, self).get_context_data(**kwargs)
+        context["version_head_lst"] = Title.objects.first().version
+        context["proj_name"] = self.proj_name
+        context["block_name"] = self.block_name
+        return context
+
+class VersionDetail(DetailView):
+    """be report version details"""
+    model = Version
+    def get_object(self):
+        self.proj_name = self.kwargs.get("proj_name")
+        self.block_name = self.kwargs.get("block_name")
+        self.version_name = self.kwargs.get("version_name")
+        return Version.objects.get(
+            name=self.version_name, block__name=self.block_name, block__proj__name=self.proj_name)
+    def get_context_data(self, **kwargs):
+        context = super(VersionDetail, self).get_context_data(**kwargs)
+        context["proj_name"] = self.proj_name
+        context["block_name"] = self.block_name
+        context["version_name"] = self.version_name
+        context['now'] = tz.now()
         return context

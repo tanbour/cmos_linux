fee52a80431eda9b16baaddc1e2a9482cbfba639
cmos3511 <gary3511@gmail.com>
2017-12-04 11:00:37 +0800

    removed gftp and added filezilla; updated op_site by using django2.0; updated iviews and vue version

--- python/op_site/be_rpt/urls.py
+++ python/op_site/be_rpt/urls.py
@@ -1,13 +1,14 @@
 """be report url"""
-from django.conf.urls import url
+from django.urls import include, path
 from . import views
 
+app_name = "be_rpt"
 urlpatterns = [
-    url(r"^show/$", views.ProjList.as_view(), name="proj_list"),
-    url(r"^show/(?P<proj_name>\w+)/$", views.BlockList.as_view(), name="block_list"),
-    url(r"^show/(?P<proj_name>\w+)/(?P<block_name>\w+)/$", views.VersionList.as_view(), name="version_list"),
-    url(r"^show/(?P<proj_name>\w+)/(?P<block_name>\w+)/(?P<version_name>\w+)/$", views.VersionDetail.as_view(), name="version_detail"),
-    url(r"^post/project/$", views.ProjPost.as_view(), name="post_project"),
-    url(r"^post/block/$", views.BlockPost.as_view(), name="post_block"),
-    # url(r"^post/version/$", views.VersionPost.as_view(), name="post_version"),
+    path("show/", views.ProjList.as_view(), name="proj_list"),
+    path("show/<proj_name>/", views.BlockList.as_view(), name="block_list"),
+    path("show/<proj_name>/<block_name>/", views.VersionList.as_view(), name="version_list"),
+    path("show/<proj_name>/<block_name>/<version_name>/", views.VersionDetail.as_view(), name="version_detail"),
+    path("post/project/", views.ProjPost.as_view(), name="post_project"),
+    path("post/block/", views.BlockPost.as_view(), name="post_block"),
+    # path("post/version/", views.VersionPost.as_view(), name="post_version"),
 ]

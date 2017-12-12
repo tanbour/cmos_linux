ff95ffb07487e9d24f968146043e4b5b4fc35c01
cmos3511 <gary3511@gmail.com>
2017-12-12 15:51:12 +0800

    using django vue component file for SPA management; followed js css python coding style to remove all possible semicolon

--- python/op_site/op_site/urls.py
+++ python/op_site/op_site/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="base_app.html")),
+    path("", TemplateView.as_view(template_name="index.html")),
     path("be_rpt/", include("be_rpt.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

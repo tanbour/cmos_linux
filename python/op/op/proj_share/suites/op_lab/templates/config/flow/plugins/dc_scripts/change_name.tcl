6f0ec41bbf0aaf4d3e4614188aa6292f80b9c841
cmos3511 <gary3511@gmail.com>
2018-02-24 11:41:50 +0800

    remoted mis-added config dir

--- python/op/op/proj_share/suites/op_lab/templates/config/flow/plugins/dc_scripts/change_name.tcl
+++
@@ -1,7 +0,0 @@
-set_fix_multiple_port_nets -all -buffer_constants [get_designs *]
-set hdlout_internal_busses true
-set bus_inference_style "%s\[%d\]"
-set verilogout_no_tri true
-#$# define_name_rules verilog -dont_change_bus_members
-define_name_rules verilog -check_bus_indexing -allowed {a-zA-Z0-9_} -remove_internal_net_bus -flatten_multi_dimension_busses -first_restricted "\\"
-change_names -rules verilog -hier -verbose
